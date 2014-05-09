cc6af892770daef4eb0c8a5074dfe471369ed89b
mreilaender <mreilaender@student.tgm.ac.at>
2014-05-09 18:48:14 +0200

    Modified all Methods, but not finished jet

--- Cipher/src/reilaender/Jtest/SubstitutionCipherTest.java
+++ Cipher/src/reilaender/Jtest/SubstitutionCipherTest.java
@@ -9,34 +9,57 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class SubstitutionCipherTest {
+	SubstitutionCipher cipher;
 	
 	@Before
-	public void setUp() {	
-		
+	public void setUp()
+	{	
+		cipher = new SubstitutionCipher("abcdefghijklmnopqrstuvwxyz");
 	}
-	
+	/*
+	 * Fall 1: Buchstabe fehlt, falsche Länge
+	 */
 	@Test(expected=IllegalArgumentException.class)
-	public void setSecretAlphabetTest1() { //Wenn falsche länge und buchstabe fehlt
-		
+	public void setSecretAlphabetTest1()
+	{
+		cipher.setSecretAlphabet(new String(MonoalphabeticCipher.STANDARD_ALPHABET).replace("a", ""));
 	}
 	
+	/*
+	 * Fall2: Richtige Länge, alle Buchstaben
+	 */
 	@Test(expected=IllegalArgumentException.class)
-	public void setSecretAlphabetTest2() { //Wenn richtige länge aber ein buchstabe fehlt
-		
+	public void setSecretAlphabetTest2()
+	{
+		cipher.setSecretAlphabet(new String(MonoalphabeticCipher.STANDARD_ALPHABET).replace('a', 'z'));
 	}
 	
+	/*
+	 * Fall 3: Falsche Länge, alle Buchstaben
+	 */
 	@Test(expected=IllegalArgumentException.class)
-	public void setSecretAlphabetTest3() { //Wenn falsche länge aber alle buchstaben
-		
+	public void setSecretAlphabetTest3()
+	{
+		cipher.setSecretAlphabet(new String(MonoalphabeticCipher.STANDARD_ALPHABET).concat("asdf"));
 	}
 	
+	/*
+	 * Fall 4: Alles korrekt
+	 */
 	@Test
-	public void setSecretAlphabetTest4() { //Wenn alles richtig...
-		
+	public void setSecretAlphabetTest4()
+	{
+		String temp = new StringBuilder(new String(MonoalphabeticCipher.STANDARD_ALPHABET)).reverse().toString();
+		cipher.setSecretAlphabet(temp);
+		temp=null;
+		System.gc();
 	}
-	
-	public void encryptTest() {//verschlüsseln
-		
+	/*
+	 * Verschlüsseln testen
+	 */
+	public void encryptTest()
+	{
+		cipher.setSecretAlphabet(new StringBuilder(new String(MonoalphabeticCipher.STANDARD_ALPHABET)).reverse().toString());
 	}
 	
 	public void enDeCryptTest() {//ver- und entschlüsseln
